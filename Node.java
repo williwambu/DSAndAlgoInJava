8a50b620b3b862553ba1c3826b84c16ff8bdd4cc
willi.wambu <willi.wambu@gmail.com>
2015-09-11 18:13:38 +0300

    Node class to represent a node in a binary tree

---
+++ Node.java
@@ -0,0 +1,12 @@
+
+public class Node {
+	int iData; //data to be used as key value
+	float fData; //other data in the node
+	
+	Node leftChild;	//this node's left child
+	Node rightChild; //this node's right child
+	
+	public void displayNode(){
+		
+	}
+}
